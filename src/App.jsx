ef1afd6040c947a99df7e960e50e3c8ad03150c9
Lucas A <lucas.gomes262@etec.sp.gov.br>
2023-01-30 18:58:47 -0300

    Alterando app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,53 @@
-import './App.css';
-import Button from './components/Button';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import TaskList from "./components/TaskList";
+import AddTask from "./components/AddTask";
+import { v4 } from "uuid";
 
 function App() {
-	return (
-		<div className="App">
-			<Button>
-				Adicionar
-			</Button>
-		</div>
-	);
+    const [tasks, setTasks] = useState([
+        {
+            id: v4(),
+            title: "Tomar umaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasassa",
+            completed: true,
+        },
+        {
+            id: v4(),
+            title: "Tomar umaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaasassa",
+            completed: true,
+        },
+    ]);
+
+    function addTask(title) {
+        const newTasks = [
+            ...tasks,
+            {
+                id: v4(),
+                title: title,
+            },
+        ];
+
+        setTasks(newTasks);
+    }
+
+    function changeTaskStatus(id) {
+        const newTasks = tasks.map((task) => {
+			if (task.id === id)
+				return { ...task, completed: !task.completed}
+
+			return task;
+        });
+
+        setTasks(newTasks);
+    }
+
+    return (
+        <div className="container">
+            <AddTask onTaskAdd={addTask} />
+            <TaskList tasks={tasks} onTaskChange={changeTaskStatus} />
+        </div>
+    );
 }
 
 export default App;
